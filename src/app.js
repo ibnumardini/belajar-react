5c0c600d0fa2043504f86046fbfdfc189117651a
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-03-22 17:36:07 +0700

    external style

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
 const root = document.querySelector("#root");
 
 const element = (
-  <div
-    style={{
-      width: 100,
-      height: 100,
-      backgroundColor: "red",
-    }}
-  ></div>
+  <div className="box"></div>
 );
 
 ReactDOM.render(element, root);
