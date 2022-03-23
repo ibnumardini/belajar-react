a1e0c36a61a3f0a806c577a4af226979ec8bef22
Muhammad Fatkurozi <fatkurozy.muhammad28@gmail.com>
2022-03-23 08:11:49 +0700

    react list, multiple rendering

--- src/app.js
+++ src/app.js
@@ -1,22 +1,18 @@
 const root = document.querySelector("#root");
 
-// react hooks, adalah fungsi2 yang di turunkan dari class component, misal this.state = useSatate dll
-// method componentDidMount = useEffect(() => {}, []) // fungsi yang akan di render pertma kali
 const App = () => {
-  const { useEffect, useRef } = React;
-
-  // ref ini untuk manipulasi dom, tanpa render ulang seperti useState()
-  const titleRef = useRef(null);
-
-  useEffect(() => {
-    setTimeout(() => {
-      titleRef.current.textContent = "Aplikasi";
-    }, 1000);
-  }, []);
+  const fruits = ["apel", "pisang", "mangga", "apel"];
 
   return (
     <>
-      <h1 ref={titleRef}>Application</h1>
+      <ul>
+        {fruits.map((fruit, idx) => {
+          // key di pakai untuk interl react memanipuasi dom treenya
+          return (
+            <li key={idx}>{fruit.charAt(0).toUpperCase() + fruit.slice(1)}</li>
+          );
+        })}
+      </ul>
     </>
   );
 };
